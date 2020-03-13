c79111533e8e1e7d44b9d83aba154c76a91517d3
aandre100 <andre@aandre100>
2020-03-13 15:19:08 +0000

    criei a classe page e as respectivas views

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -4,18 +4,35 @@ namespace Hcode;
 use Rain\Tpl;
 
 class Page {
+	private $tpl;
+	private $options = [];
+	private $defaults = [
+			"data" =>[]
+	];
 
-	public function __construct(){
+	public function __construct($opts = array()){
+		$this->options = array_merge($this->defaults, $opts);
 		$config = array(
 			"tpl_dir"   => $_SERVER["DOCUMENT_ROOT"]. "/views/",
 			"cache_dir" => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
 			"debug"     => false //set to be false to improve speed
 			);
 		Tpl::configure( $config );
-		$tpl = new Tpl();
+		$this->tpl = new Tpl;
+		$this->setData($this->options["data"]);
+		$this->tpl->draw("header");
+	}
+	private function setData($data = array()) {
+		foreach($data as $key => $value) {
+			$this->tpl->assign($key, $value);
+		}
+	}
+	public function setTpl($name, $data = array(), $returnHTML = false){
+		$this->setData($data);
+		return $this->tpl->draw($name, $returnHTML);
 	}
 	public function __destruct(){
-
+		$this->tpl->draw("footer");
 	}
 
 }
