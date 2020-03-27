78820bd81b8bd2ccf06655bb10f8ffa0baf131ae
André Cardoso <andre@spzn.pt>
2020-03-27 16:45:17 +0000

    site produtos front

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ $app->config('debug', true);
 
 
 require_once("site.php");
+require_once("functions.php");
 require_once("admin.php");
 require_once("admin-user.php");
 require_once("forget.php");
--- site.php
+++ site.php
@@ -1,17 +1,18 @@
 <?php
 
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 
 $app->get('/', function() {
+	$products = Product::listAll();
 
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		'products' => Product::checkList($products)
+	]);
 
-	// $sql = new Hcode\DB\Sql();
-	// $results = $sql->select("SELECT * FROM tb_users");
-	// echo json_encode($results);
 
 });
 
